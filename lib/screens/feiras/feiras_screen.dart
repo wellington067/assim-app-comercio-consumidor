a8b24a4f7e657b2e81eaff4c25e5fb9f42fafb7f
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-04-16 18:26:00 -0300

    fix: listagem de feiras, passando apenas as feiras filtradas pela cidade.

--- lib/screens/feiras/feiras_screen.dart
+++ lib/screens/feiras/feiras_screen.dart
@@ -8,20 +8,19 @@ import 'package:ecommerceassim/shared/core/controllers/feira_controller.dart';
 import 'package:ecommerceassim/shared/core/models/feira_model.dart';
 
 class FeirasScreen extends StatelessWidget {
-  const FeirasScreen({super.key});
+  const FeirasScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
     final Map<String, dynamic> arguments =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic> ??
             {};
+    final int cidadeId = arguments['id'] as int;
     final String cidadeNome = arguments['nome'] as String? ?? 'Cidade';
 
-    final feiraController = Provider.of<FeiraController>(context);
-
-    if (feiraController.feiras.isEmpty) {
-      feiraController.loadFeiras();
-    }
+    final feiraController =
+        Provider.of<FeiraController>(context, listen: false);
+    feiraController.loadFeirasByCidadeId(cidadeId);
 
     return Scaffold(
       appBar: const CustomAppBar(),
@@ -48,76 +47,78 @@ class FeirasScreen extends StatelessWidget {
           Expanded(
             child: Consumer<FeiraController>(
               builder: (context, feiraController, child) {
-                List<FeiraModel> feiras = feiraController.feiras;
-
-                if (feiras.isEmpty) {
-                  return const Center(
+                if (feiraController.isLoading) {
+                  // Se ainda estiver carregando, exiba o indicador de progresso
+                  return Center(
                     child: CircularProgressIndicator(color: kDetailColor),
                   );
-                }
+                } else {
+                  // Se o carregamento estiver completo, exiba a lista de feiras
+                  List<FeiraModel> feiras = feiraController.feiras;
 
-                return ListView.builder(
-                  itemCount: feiras.length,
-                  itemBuilder: (context, index) {
-                    FeiraModel feira = feiras[index];
+                  return ListView.builder(
+                    itemCount: feiras.length,
+                    itemBuilder: (context, index) {
+                      FeiraModel feira = feiras[index];
 
-                    return Container(
-                      margin: const EdgeInsets.all(7.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15.0),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.3),
-                            spreadRadius: 2,
-                            blurRadius: 6,
-                            offset: const Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: Material(
-                        color: Colors.transparent,
-                        borderRadius: BorderRadius.circular(15.0),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.pushNamed(context, Screens.bancas,
-                                arguments: {
-                                  'id': feira.id,
-                                  'nome': feira.nome,
-                                  'bairro': feira.bairroId,
-                                  'horarios': feira.horariosFuncionamento,
-                                });
-                          },
+                      return Container(
+                        margin: const EdgeInsets.all(7.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
                           borderRadius: BorderRadius.circular(15.0),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Row(
-                              children: [
-                                const CircleAvatar(
-                                  radius: 25.0,
-                                  backgroundImage: AssetImage(
-                                    "lib/assets/images/banca-fruta.jpg",
-                                  ),
-                                ),
-                                const SizedBox(width: 16.0),
-                                Expanded(
-                                  child: Text(
-                                    feira.nome,
-                                    style: const TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.bold,
-                                      color: kTextColorBlack,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 2,
+                              blurRadius: 6,
+                              offset: const Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: Material(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(15.0),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(context, Screens.bancas,
+                                  arguments: {
+                                    'id': feira.id,
+                                    'nome': feira.nome,
+                                    'bairro': feira.bairroId,
+                                    'horarios': feira.horariosFuncionamento,
+                                  });
+                            },
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Row(
+                                children: [
+                                  const CircleAvatar(
+                                    radius: 25.0,
+                                    backgroundImage: AssetImage(
+                                      "lib/assets/images/banca-fruta.jpg",
                                     ),
                                   ),
-                                ),
-                              ],
+                                  const SizedBox(width: 16.0),
+                                  Expanded(
+                                    child: Text(
+                                      feira.nome,
+                                      style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        color: kTextColorBlack,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    );
-                  },
-                );
+                      );
+                    },
+                  );
+                }
               },
             ),
           ),
--- lib/shared/core/controllers/feira_controller.dart
+++ lib/shared/core/controllers/feira_controller.dart
@@ -1,11 +1,14 @@
+// ignore_for_file: avoid_print
+
 import 'package:dio/dio.dart';
+import 'package:ecommerceassim/shared/constants/app_text_constants.dart';
 import 'package:ecommerceassim/shared/core/controllers/bairro_controller.dart';
 import 'package:ecommerceassim/shared/core/models/feira_model.dart';
 import 'package:ecommerceassim/shared/core/repositories/feira_repository.dart';
+import 'package:ecommerceassim/shared/core/user_storage.dart';
 import 'package:flutter/material.dart';
 
 class FeiraController with ChangeNotifier {
-  final FeiraRepository _feiraRepository = FeiraRepository(Dio());
   final BairroController _bairroController;
 
   FeiraController(this._bairroController);
@@ -14,12 +17,86 @@ class FeiraController with ChangeNotifier {
 
   List<FeiraModel> get feiras => _feiras;
 
+  final bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
   Future<void> loadFeiras() async {
+    Dio dio = Dio();
+
+    FeiraRepository feiraRepository = FeiraRepository(dio);
+
     try {
-      _feiras = await _feiraRepository.getFeiras();
+      _feiras = await feiraRepository.getFeiras();
       notifyListeners();
     } catch (error) {
       print('Erro ao carregar as feiras: $error');
+    }
+  }
+
+  Future<void> loadFeirasByCidadeId(int cidadeId) async {
+    UserStorage userStorage = UserStorage();
+    Dio dio = Dio();
+    String userToken = await userStorage.getUserToken();
+    try {
+      var bairrosResponse = await dio.get(
+        '$kBaseURL/bairros/cidade/$cidadeId',
+        options: Options(
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            'Cache-Control': 'no-cache',
+            "Authorization": "Bearer $userToken"
+          },
+        ),
+      );
+
+      if (bairrosResponse.statusCode == 200) {
+        final bairrosJson = List.from(bairrosResponse.data['bairros']);
+        final List<int> bairroIds =
+            bairrosJson.map((bairro) => bairro['id'] as int).toList();
+
+        // print("IDs dos Bairros: $bairroIds");
+
+        var feirasResponse = await dio.get(
+          '$kBaseURL/feiras',
+          options: Options(
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json",
+              'Cache-Control': 'no-cache',
+              "Authorization": "Bearer $userToken"
+            },
+          ),
+        );
+
+        if (feirasResponse.statusCode == 200) {
+          final feirasJson = List.from(feirasResponse.data['feiras'])
+              .map((feira) => FeiraModel.fromJson(feira))
+              .toList();
+
+          print(
+              "Feiras antes da filtragem: ${feirasJson.map((feira) => feira.bairroId).toList()}");
+
+          _feiras = feirasJson
+              .where((feira) => bairroIds.contains(feira.bairroId))
+              .toList();
+
+          print(
+              "Feiras após a filtragem: ${_feiras.map((feira) => feira.bairroId).toList()}");
+
+          notifyListeners();
+        } else {
+          print('Erro ao carregar feiras: ${feirasResponse.statusCode}');
+          throw Exception('Failed to load feiras');
+        }
+      } else {
+        print('Erro ao carregar bairros: ${bairrosResponse.statusCode}');
+        throw Exception('Failed to load bairros');
+      }
+    } catch (error) {
+      print('Erro ao carregar as feiras e bairros: $error');
+      rethrow;
     }
   }
 
