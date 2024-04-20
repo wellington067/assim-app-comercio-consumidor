d590f45842e762f22b77452aaa4c0e5eac4dab58
tiagojose76 <tiagojose76@gmail.com>
2024-04-20 00:24:01 -0300

    feat: loading entre tela de cidade para feira

--- lib/screens/feiras/feiras_screen.dart
+++ lib/screens/feiras/feiras_screen.dart
@@ -1,12 +1,11 @@
-import 'package:ecommerceassim/components/appBar/custom_app_bar.dart';
-import 'package:ecommerceassim/components/buttons/custom_search_field.dart';
-import 'package:ecommerceassim/shared/components/BottomNavigation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:ecommerceassim/screens/screens_index.dart';
 import 'package:ecommerceassim/shared/constants/style_constants.dart';
 import 'package:ecommerceassim/shared/core/controllers/feira_controller.dart';
 import 'package:ecommerceassim/shared/core/models/feira_model.dart';
+import 'package:ecommerceassim/components/buttons/custom_search_field.dart';
+import 'package:ecommerceassim/components/appBar/custom_app_bar.dart';
 
 class FeirasScreen extends StatelessWidget {
   const FeirasScreen({Key? key});
@@ -17,22 +16,16 @@ class FeirasScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic> ??
             {};
     final int cidadeId = arguments['id'] as int;
-    final String cidadeNome = arguments['nome'] as String? ?? 'Cidade';
-
-    final feiraController =
-        Provider.of<FeiraController>(context, listen: false);
-    feiraController.loadFeirasByCidadeId(cidadeId);
+    final String cidadeNome = arguments['nome'] as String;
 
     return Scaffold(
       appBar: const CustomAppBar(),
       backgroundColor: Colors.white,
-      bottomNavigationBar: BottomNavigation(selectedIndex: 0),
-
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: const EdgeInsets.fromLTRB(25.0, 16.0, 16.0,10.0),
+            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 16.0),
             child: Text(
               'Feiras em $cidadeNome',
               style: const TextStyle(
@@ -44,20 +37,28 @@ class FeirasScreen extends StatelessWidget {
           const CustomSearchField(
             fillColor: kOnBackgroundColorText,
             iconColor: kDetailColor,
-            hintText: 'Buscar por feiras',
-            padding: EdgeInsets.all(21),
+            hintText: 'Buscar',
+            padding: EdgeInsets.all(21.0),
           ),
           Expanded(
-            child: Consumer<FeiraController>(
-              builder: (context, feiraController, child) {
-                if (feiraController.isLoading) {
-                  // Se ainda estiver carregando, exiba o indicador de progresso
+            child: FutureBuilder(
+              future: Provider.of<FeiraController>(context, listen: false)
+                  .loadFeirasByCidadeId(cidadeId),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
                   return const Center(
-                    child: CircularProgressIndicator(color: kDetailColor),
+                    child: CircularProgressIndicator(
+                      color: kDetailColor,
+                      backgroundColor: Colors.white,
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Erro: ${snapshot.error}'),
                   );
                 } else {
-                  // Se o carregamento estiver completo, exiba a lista de feiras
-                  List<FeiraModel> feiras = feiraController.feiras;
+                  List<FeiraModel> feiras =
+                      Provider.of<FeiraController>(context).feiras;
 
                   return ListView.builder(
                     itemCount: feiras.length,
@@ -66,7 +67,7 @@ class FeirasScreen extends StatelessWidget {
 
                       return Container(
                         margin: const EdgeInsets.symmetric(
-                            horizontal: 22.0,vertical:8.0),
+                            horizontal: 22.0, vertical: 8.0),
                         decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(15.0),
@@ -93,9 +94,8 @@ class FeirasScreen extends StatelessWidget {
                                   });
                             },
                             borderRadius: BorderRadius.circular(15.0),
-                            
                             child: Padding(
-                              padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0,16.0),
+                              padding: const EdgeInsets.all(16.0),
                               child: Row(
                                 children: [
                                   const CircleAvatar(
