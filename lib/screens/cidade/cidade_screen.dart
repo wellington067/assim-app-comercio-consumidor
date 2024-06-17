473a54a940984451237530ad2cc5360f28ca4776
Henrique Almeida <henriquesilvabcc@gmail.com>
2024-06-17 19:58:39 -0300

    fix: padding das mensagens de erro das cidades, feiras, bancas e produtos.

--- lib/screens/cidade/cidade_screen.dart
+++ lib/screens/cidade/cidade_screen.dart
@@ -149,7 +149,7 @@ class _CidadeScreenState extends State<CidadeScreen> {
   Widget _buildEmptyListWidget() {
     return Center(
       child: Padding(
-        padding: const EdgeInsets.only(top: 00.0),
+        padding: const EdgeInsets.only(top: 00.0, left: 8, right: 8),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
